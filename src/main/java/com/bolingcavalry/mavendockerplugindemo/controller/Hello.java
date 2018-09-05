a606e042f0617beb955855e297fcf50831f77269
Martin-fan <13579275375@sina.cn>
2018-09-05 08:51:16 +0800

    修改hello

--- src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
+++ src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
@@ -1,16 +1,10 @@
 ﻿package com.bolingcavalry.mavendockerplugindemo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
-
-@RestController
 public class Hello {
 
-    @RequestMapping("/")
-    public String sayHello(){
-    	//修改返回的字符串的内容
-        return "啊啊啊啊啊啊. Hello jenkins, " + new Date();
+    
+    public static void main(String[] args){
+	
+	System.out.println("hello world!"); 
     }
 }
